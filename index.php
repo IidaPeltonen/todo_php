a5ccfe2421949b01dd23ea2869d9f515f21081f4
IidaPeltonen <n0peii00@students.oamk.fi>
2021-10-18 21:24:19 +0300

    ok

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin:'
 header('Content-Type: application/json');
 
 $db = new PDO('mysql:host=localhost;dbname=todo;charset=utf8', 'root', '');
-$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+$sql = "select * from task"
 $query = $db->query($sql);
 $results  = $query->fetchAll(PDO::FETCH_ASSOC);
 header('HTTP/1.1 200 OK');
