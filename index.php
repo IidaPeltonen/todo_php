fd71a19f617431fc87e064be5d77bafa6be2e20d
IidaPeltonen <n0peii00@students.oamk.fi>
2021-10-18 21:02:48 +0300

    ok

--- index.php
+++ index.php
@@ -1,50 +1,10 @@
 <?php
-header('Access-Control-Allow-Origin:' . $_SERVER['HTTP_ORIGIN']);
-header('Access-Control-Allow-Credentials;true');
-header('Access-Control-Allow-Methods: GET, POST, OPTIO;N');
-header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Max-Age: 3600');
+header('Access-Control-Allow-Origin:' 
 header('Content-Type: application/json');
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    return 0;
-}
-
-$input = json_decode(file_get_contents('php://input'));
-$description = filter_var($input->description,FILTER_SANITIZE_STRING);
-
-try {
-    $db = new PDO('mysql:host=localhost;dbname=todo;charset=utf8', 'root', '');
-    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-    $query = $db->prepare('Insert into task(description) values (:description)');
-    $query->bindValue(':description', $description, PDO::PARAM_STR);
-    $query->execute();
-
-    header('HTTP/1.1 200 OK');
-    $data = array('id' => $db->lastInsertId(), 'description' => $description);
-    print json_encode($data);  
-} catch (PDOException $pdoex) {
-    header('HTTP/1.1 500 Internal Server Error');
-    $error = array('error' => $pdoex->getMessage());
-    print json_encode($error);
-} 
-
-
-/*     $sql = "select * from task";
-    $query = $db->query($sql);
-    $results  = $query->fetchAll(PDO::FETCH_ASSOC);
-    header('HTTP/1.1 200 OK');
-    print json_encode($results);
-} catch (PDOException $pdoex) {
-    header('HTTP/1.1 500 Internal Server Error');
-    $error = array('error' => $pdoex->getMessage());
-    print json_encode($error);
-} */
-
-/* $db = new PDO('mysql:host=localhost;dbname=todo;charset=utf8', 'root', '');
-$sql = "select * from task";
+$db = new PDO('mysql:host=localhost;dbname=todo;charset=utf8', 'root', '');
+$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 $query = $db->query($sql);
 $results  = $query->fetchAll(PDO::FETCH_ASSOC);
 header('HTTP/1.1 200 OK');
-print json_encode($results); */
+print json_encode($results); 
